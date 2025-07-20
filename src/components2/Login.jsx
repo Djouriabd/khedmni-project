c00f9749309593927de16693af40c9e2e9593924
djouri <rubyaichan@gmail.com>
2025-07-20 15:34:15 +0100

    add path to sign up page +add responsive

--- src/components2/Login.jsx
+++ src/components2/Login.jsx
@@ -1,5 +1,6 @@
 import './Login.css'
 import logoKhedmni from '../assets/khdemni1.svg'
+import { Link } from 'react-router-dom'
 
 function Login(){
 
@@ -8,12 +9,18 @@ function Login(){
             <img src={logoKhedmni} alt="logo"/>
             <form className='login-form'>
                 <h2 id='h2'>Login</h2>
-                <label className='label-login' htmlFor="email">enter your name or email*</label>
-                <input type="email" placeholder='Username/Email'/>
-                <label className='label-login' htmlFor="email">enter your password*</label>
-                <input type="email" placeholder='Password'/>
+                <div className='form-group'>
+                    <label className='label-login' htmlFor="email">enter your name or email*</label>
+                    <input type="email" id="email" placeholder='Username/Email'/>
+                </div>
+                
+                <div className='form-group'>
+                    <label className='label-login' htmlFor="password">enter your password*</label>
+                    <input type="password" id="password" placeholder='Password'/>
+                </div>
+                
                 <button type='submit' className='submit'>Login</button>
-                <p className='sign'>Don't have an account? <a href="#">Sign Up</a></p>
+                <p className='sign'>Don't have an account? <Link to="/signup">Sign Up</Link></p>
             </form>
         </div>
     )
