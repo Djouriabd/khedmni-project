0ede29d97f1adf24d6964ed3522f047572053f88
djouri <rubyaichan@gmail.com>
2025-07-20 15:40:09 +0100

    fix some errors + add path to login page + add responsive

--- src/components2/Signup.jsx
+++ src/components2/Signup.jsx
@@ -1,5 +1,6 @@
 import logoKhedmni from '../assets/khdemni1.svg';
 import './Signup.css'
+import { Link } from 'react-router-dom'
 
 function Signup(){
 
@@ -8,20 +9,30 @@ function Signup(){
             <img src={logoKhedmni} alt="logo"/>
             <form className='sign-form'>
                 <h2 id='hh'>Sign Up</h2>
-                <div className='name'>
-                    <label htmlFor="email">name*</label>
-                    <input type="email" placeholder='Name'/>
+
+                <div className='name-row'>
+                    <div className='name'>
+                        <label htmlFor="name">name*</label>
+                        <input type="text" id="name" placeholder='Name'/>
+                    </div>
+                    <div className='name'>
+                        <label htmlFor="surname">surname*</label>
+                        <input type="text" id="surname" placeholder='Surname'/>
+                    </div>
                 </div>
-                <div className='name'>
-                    <label htmlFor="email">surname*</label>
-                    <input type="email" placeholder='Username'/>
+                
+                <div className='form-group'>
+                    <label htmlFor="email">enter your email*</label>
+                    <input type="email" id="email" placeholder='Email'/>
                 </div>
-                <label htmlFor="email">enter your email*</label>
-                <input type="email" placeholder='Email'/>
-                <label htmlFor="email">enter your password*</label>
-                <input type="email" placeholder='Password'/>
-                <button type='submit' className='submit'>Login</button>
-                <p className='sign'>Don't have an account? <a href="#">Sign Up</a></p>
+                
+                <div className='form-group'>
+                    <label htmlFor="password">enter your password*</label>
+                    <input type="password" id="password" placeholder='Password'/>
+                </div>
+                
+                <button type='submit' className='submit'>Sign Up</button>
+                <p className='sign'>Already have an account? <Link to="/login">Login</Link></p>
             </form>
         </div>
     )
